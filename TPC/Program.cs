c43906b1d59acd186a21df136c18c2d02c350176
randelramirez <randelramirez1@gmail.com>
2016-07-24 02:06:43 +0800

    Cleanup

--- TPC/Program.cs
+++ TPC/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TPC
 {
-   class Program
+    class Program
    {
       static void Main( string[] args )
       {
